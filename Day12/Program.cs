5c340b1f44ccbf75406704dd7cc71fbdffa049c6
Daniel Rasmusson <daniel.rasmusson@hogia.se>
2021-12-16 10:27:44 +0100

    December 12 (finally).

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,78 +1,110 @@
 ﻿//https://adventofcode.com/2021/day/12
+
 PartOne();
-Console.WriteLine("Hello, World!");
+PartTwo();
 
 void PartOne()
 {
-    var input = File.ReadAllLines("Day12/1.txt");
-    var caves = input.SelectMany(x => x.Split('-')).ToHashSet().Select(i => new Cave(i)).ToList();
+    var input = File.ReadAllLines("Day12/1.txt").ToList();
+    var caveMap = new Dictionary<string, List<Cave>>();
+    input.SelectMany(x => x.Split("-"))
+        .Distinct().ToList().
+        ForEach(caveName =>
+        {
+            var neighbouringCaves = input.Where(x => x.Split('-')[0] == caveName || x.Split('-')[1] == caveName).SelectMany(x => x.Split('-')).Where(x => x != caveName).Select(x => CreateCave(x)).ToList();
+            caveMap.Add(caveName, neighbouringCaves);
+        });
+    
+    var log = new List<string>();
+    Traverse(new Start(), new List<Cave>());
+    Console.WriteLine(log.Distinct().ToList().Count);
 
-    foreach (var connection in input.Select(x => x.Split('-')))
+    void Traverse(Cave current, List<Cave> visited)
     {
-        caves.First(c => c.Name == connection[0]).AddConnectedCave(caves.First(x => x.Name == connection[1]));
-        caves.First(c => c.Name == connection[1]).AddConnectedCave(caves.First(x => x.Name == connection[0]));
+        visited.Add(current);
+        if (current is End)
+        {
+            log.Add(string.Join(",", visited.Select(x => x.Name)));
+            visited = new();
+            return;
+        }
+
+        var nextCaves = caveMap[current.Name].Where(x => x.IsLargeCave || x is End || !visited.Contains(x) && x is not Start).ToList();
+
+        foreach (var cave in nextCaves)
+        {
+            Traverse(cave, visited.ToList());
+        }
     }
-    var start = caves.First(c => c.Start);
-    var result = 0;
-    var t = start.Traverse(result);
-    Console.WriteLine(t);
 }
 
-internal class Cave
+void PartTwo()
 {
-    public string Name { get; private set; }
-    public bool IsSmallCave { get; private set; }
-    public bool Start { get; private set; }
-    public bool End { get; private set; }
-    public List<Cave> ConnectedCaves { get; private set; }
-    public bool CantBeTraversed => (Start && TimesTraversed > 0) || (IsSmallCave && TimesTraversed > 0);
-    private int TimesTraversed;
-
-    public Cave(string name)
-    {
-        Name = name;
-
-        switch (name)
+    var input = File.ReadAllLines("Day12/1.txt").ToList();
+    var caveMap = new Dictionary<string, List<Cave>>();
+    input.SelectMany(x => x.Split("-"))
+        .Distinct().ToList().
+        ForEach(caveName =>
         {
-            case "start":
-                Start = true;
-                break;
-            case "end":
-                End = true;
-                break;
-            default:
-                break;
-        }
-        if (name.Length == 1 && char.IsLower(name[0]))
-            IsSmallCave = true;
+            var neighbouringCaves = input.Where(x => x.Split('-')[0] == caveName || x.Split('-')[1] == caveName).SelectMany(x => x.Split('-')).Where(x => x != caveName).Select(x => CreateCave(x)).ToList();
+            caveMap.Add(caveName, neighbouringCaves);
+        });
 
-        ConnectedCaves = new List<Cave>();
-    }
+    var log = new List<string>();
+    Traverse(new Start(), new List<Cave>());
+    Console.WriteLine(log.Distinct().ToList().Count);
 
-    public void AddConnectedCave(Cave cave)
-    {
-        ConnectedCaves.Add(cave);
-    }
-
-    public int Traverse(int numberOfRoutesToEnd)
-    {
-        if (!End)
+    void Traverse(Cave current, List< Cave > visited)
+{
+        visited.Add(current);
+        if (current is End)
         {
-            
-            TimesTraversed++;
-            if (ConnectedCaves.All(cc => cc.CantBeTraversed))
-            {
-                return 0;
-            }
-            foreach (var connectedCave in ConnectedCaves)
-            {
-                if (!connectedCave.CantBeTraversed)
-                {
-                    numberOfRoutesToEnd += connectedCave.Traverse(numberOfRoutesToEnd);
-                }
-            }
+            log.Add(string.Join(",", visited.Select(x => x.Name)));
+            visited = new();
+            return;
         }
-        numberOfRoutesToEnd++;
-        return numberOfRoutesToEnd;
+
+        var multipleSmallVisits = visited.Any(x => !x.IsLargeCave && visited.Count(y => y == x) == 2);
+
+        var nextCaves = multipleSmallVisits
+            ? caveMap[current.Name].Where(x => x.IsLargeCave || x is End || !visited.Contains(x) && x is not Start).ToList()
+            : caveMap[current.Name].Where(x => x is not Start).ToList();
+
+        foreach (var cave in nextCaves)
+        {
+            Traverse(cave, visited.ToList());
+        }
+    }
+}
+
+Cave CreateCave(string name)
+{
+    switch (name)
+    {
+        case "start":
+            return new Start(name);
+        case "end":
+            return new End(name);
+        default:
+            return new Cave(name);
+    }
+}
+
+internal record Cave(string Name)
+{
+    public bool IsLargeCave => Name.ToUpper() == Name;
+}
+
+internal record Start : Cave
+{
+    public Start(string Name = "start") : base(Name)
+    {
+    }
+}
+
+internal record End : Cave
+{
+    public End(string Name = "end") : base(Name)
+    {
     }
 }
